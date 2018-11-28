e991fd939b76d40be732431c39528c6ef21ecfd2
loiPH <loiph.vncoder@users.noreply.github.com>
2018-11-28 16:32:17 +0700

    update

--- local/resources/views/frontend/gioithieu/g_1.blade.php
+++ local/resources/views/frontend/gioithieu/g_1.blade.php
@@ -1,8 +1,8 @@
 <div class="container-fluid" id="g_1" style="background-image:url({{URL::asset('images/bg/bg-full.png')}});">
     <div class="container">
         <div class="row">
-            <div class="col-md-9">
-                <h1>{{$data['post']->title}}</h1>
+            <div class="col-md-9 bg-white shadow-sm">
+                <h1 class="p-3">{{$data['post']->title}}</h1>
                 {{--<h4>NỘI DUNG TRANG BẢNG GIÁ để làm màu xanh và font khác</h4>--}}
                 {!! $data['post']->content !!}
             </div>
