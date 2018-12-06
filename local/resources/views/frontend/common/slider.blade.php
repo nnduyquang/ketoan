823bec7e57798426c8d2ed3da9fa5aa37e29385f
loiPH <loiph.vncoder@users.noreply.github.com>
2018-12-06 15:04:46 +0700

    update

--- local/resources/views/frontend/common/slider.blade.php
+++ local/resources/views/frontend/common/slider.blade.php
@@ -3,39 +3,42 @@
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <div class="img-slider" style="background-image:url({{URL::asset('images/slider/slide_2.jpg')}});">
-                    <div class="carousel-caption text-left d-none d-md-block">
-                        <h5 class="animated zoomIn slow">THÀNH LẬP DOANH NGHIỆP</h5>
-                        <h6 class="animated zoomIn slow" style="animation-delay: 300ms">
-                            KẾ TOÁN THUẾ
-                        </h6>
-                        <p class="animated zoomIn slow" style="animation-delay: 500ms;font-size: 32px;">
-                            Miễn phí <span style="font-size: 33px;font-weight: bold">3</span> tháng báo cáo thuế.
-                        </p>
-                        <p class="animated zoomIn slow desc" style="animation-delay: 600ms">
-                            Tư vấn và đưa ra giải pháp về luật, thủ tục tuân thủ,
-                            kế toán tài chính và điều hành.<br>
-                            Phát triển kinh doanh cốt lõi.
-                        </p>
-                        <button class="animated zoomIn" style="animation-delay: 1s">XEM CHI TIẾT</button>
+                    <div class="carousel-caption text-left d-none d-md-block" style="left: 10%;z-index: 20">
+                        <div style="background-color:rgba(00,00,00,0.7);width: 500px" class="p-4">
+                            <h5 class="animated zoomIn slow">THÀNH LẬP DOANH NGHIỆP</h5>
+                            <h6 class="animated zoomIn slow" style="animation-delay: 300ms">
+                                KẾ TOÁN THUẾ
+                            </h6>
+                            <p class="animated zoomIn slow" style="animation-delay: 500ms;font-size: 28px;">
+                                Miễn phí <span style="font-size: 33px;font-weight: bold">3</span> tháng báo cáo thuế.
+                            </p>
+                            <p class="animated zoomIn slow desc" style="animation-delay: 600ms">
+                                Tư vấn và đưa ra giải pháp về luật, thủ tục tuân thủ,
+                                kế toán tài chính và điều hành.<br>
+                                {{--Phát triển kinh doanh cốt lõi.--}}
+                            </p>
+                            <button class="animated zoomIn" style="animation-delay: 1s">XEM CHI TIẾT</button>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="carousel-item">
-                <div class="img-slider" style="background-image:url({{URL::asset('images/slider/2.jpg')}});">
+                <div class="img-slider" style="background-image:url({{URL::asset('images/slider/4.jpg')}});">
 
-                    <div class="carousel-caption text-left d-none d-md-block">
-                        <h5 class="animated zoomIn slow">GIẢI PHÁP DOANH NGHIỆP</h5>
-                        <h6 class="animated zoomIn slow" style="animation-delay: 300ms">KẾ TOÁN THUẾ</h6>
-                        <p class="animated zoomIn slow" style="animation-delay: 500ms;font-size: 32px;">
-                            Miễn phí <span style="font-size: 33px;font-weight: bold">3</span> tháng báo cáo thuế.
-                        </p>
-                        <p class="animated zoomIn slow desc" style="animation-delay: 600ms">
-                            Giải pháp nghiệp vụ kế toán thuế cho doanh nghiệp <br>
-                            Chuyên Nghiệp-Minh Bạch-Hiệu Quả
-                        </p>
-                        <button class="animated zoomIn" style="animation-delay: 1s">XEM CHI TIẾT</button>
+                    <div class="carousel-caption text-left d-none d-md-block" style="left: 10%;z-index: 20">
+                        <div style="background-color:rgba(00,00,00,0.7);width: 500px" class="p-4">
+                            <h5 class="animated zoomIn slow">GIẢI PHÁP DOANH NGHIỆP</h5>
+                            <h6 class="animated zoomIn slow" style="animation-delay: 300ms">KẾ TOÁN THUẾ</h6>
+                            <p class="animated zoomIn slow" style="animation-delay: 500ms;font-size:  28px;">
+                                Miễn phí <span style="font-size: 33px;font-weight: bold">3</span> tháng báo cáo thuế.
+                            </p>
+                            <p class="animated zoomIn slow desc" style="animation-delay: 600ms">
+                                Giải pháp nghiệp vụ kế toán thuế cho doanh nghiệp <br>
+                                Chuyên Nghiệp-Minh Bạch-Hiệu Quả
+                            </p>
+                            <button class="animated zoomIn" style="animation-delay: 1s">XEM CHI TIẾT</button>
+                        </div>
                     </div>
-
                 </div>
             </div>
             {{--<div class="carousel-item">--}}
